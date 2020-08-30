8d275aa3fbe064b22c63419f7d01cf07854f4758
joilson <joilson_rg@hotmail.com>
2020-08-30 18:43:58 -0300

    feature: adiciona header component e style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.css';
+
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Youtube clone</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
+
+import './styles.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <MenuIcon />
+                <img 
+                    className="header__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/250px-YouTube_Logo_2017.svg.png" 
+                    alt="youtube" 
+                />
+            </div>
+
+            <div className="header__input">
+                <input placeholder="Search" type="text"/>
+                <SearchIcon className="header__inputButton" />
+            </div>
+
+            <div className="header__icons">
+                <VideoCallIcon className="header__icon"/>
+                <AppsIcon className="header__icon"/>
+                <NotificationsIcon className="header__icon"/>
+                <Avatar 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThLphY2_lhKMbjeJ6xhZMqUvjRSDxjv6EhnQ&usqp=CAU"
+                    alt="Albert Einstein"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
