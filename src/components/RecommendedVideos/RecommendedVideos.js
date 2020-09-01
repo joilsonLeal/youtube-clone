876f6525cf51bb543de1770f3e221ae785b07486
joilson <joilson_rg@hotmail.com>
2020-09-01 09:34:08 -0300

    feature: adiciona e estiliza RecommendedVideos component

---
+++ src/components/RecommendedVideos/RecommendedVideos.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import VideoCard from '../VideoCard/VideoCard';
+
+import './styles.css';
+
+function RecommendedVideos() {
+    return (
+        <div className="recommendedVideos">
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+                <VideoCard 
+                    title="Let's Build a YouTube Clone with REACT JS for Beginners"
+                    views="84.716 Views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+                    channel="Clever Programmer"
+                    image="https://i.ytimg.com/vi/GePLvNgWROg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAjwvR4sTpsiqednKI2jP7Eg9KO9g"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default RecommendedVideos;
