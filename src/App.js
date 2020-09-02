70e90de4fa82161ae73e4ff23b2fcf504ebdba39
joilson <joilson_rg@hotmail.com>
2020-09-02 08:38:36 -0300

    feature: adiciona roteamento entre os componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import RecommendedVideos from './components/RecommendedVideos';
 
+import './App.css';
+
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
+      <Router>
+        <Header />
+
+        <Switch>
+
+          <Route path="/" exact>
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+          
+          <Route path="/search/:search_query">
+
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
@@ -10,20 +11,32 @@ import Avatar from '@material-ui/core/Avatar';
 import './styles.css';
 
 function Header() {
+    const [inputSearch, setInputSearch] = useState('');
+
+
     return (
         <div className="header">
             <div className="header__left">
                 <MenuIcon />
-                <img 
-                    className="header__logo"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/250px-YouTube_Logo_2017.svg.png" 
-                    alt="youtube" 
-                />
+                <Link to="/">
+                    <img 
+                        className="header__logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/250px-YouTube_Logo_2017.svg.png" 
+                        alt="youtube" 
+                    />
+                </Link>
             </div>
 
             <div className="header__input">
-                <input placeholder="Search" type="text"/>
-                <SearchIcon className="header__inputButton" />
+                <input 
+                    placeholder="Search" 
+                    type="text"
+                    value={ inputSearch } 
+                    onChange={ e => setInputSearch(e.target.value) }
+                />
+                <Link to={`/search/${inputSearch}`}>
+                    <SearchIcon className="header__inputButton" />
+                </Link>
             </div>
 
             <div className="header__icons">
