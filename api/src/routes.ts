343c2d15efc6bb7a8081b32cf25fbf85d1dd0e06
joilson <joilson_rg@hotmail.com>
2020-09-09 07:07:16 -0300

    feature: cria controller para users

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,22 +1,9 @@
 import express from 'express';
-import db from './database/connection';
+
+import UsersController from './controllers/UsersController';
 
 const routes = express.Router();
 
-routes.get('/',  (req, res) => {
-    return res.json({ message: 'youtube-clone' });
-});
-
-routes.post('/users', async (req, res) => {
-    const { username, password, avatar } = req.body;
-
-    await db('users').insert({
-        username,
-        password,
-        avatar,
-    });
-
-    return res.json({ok: 'ok'});
-});
+routes.post('/users', UsersController.create);
 
 export default routes;
