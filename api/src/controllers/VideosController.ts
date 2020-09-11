9f8c1a4ac909814f120808d4c49b1141dc22f35e
joilson <joilson_rg@hotmail.com>
2020-09-11 15:17:12 -0300

    feature: adiciona função create nos vídeos

--- api/src/controllers/VideosController.ts
+++ api/src/controllers/VideosController.ts
@@ -6,7 +6,35 @@ class VideosController {
     async index(request: Request, response: Response) {
         const videos = await db.from('videos').select('*');
 
-        response.send(videos);
+        return response.json(videos);
+    }
+
+    async create(request: Request, response: Response) {
+        const { 
+            name,
+            description,
+            url,
+            thumbnail,
+            views,
+            created_at,
+            channel_id
+        } = request.body;
+        
+        try {
+            await db.insert({
+                name,
+                description,
+                url,
+                thumbnail,
+                views,
+                created_at,
+                channel_id
+            });
+        } catch {
+            return response.status(400).json({ message: "Erro ao inserir vídeo." });
+        }
+        
+        return response.status(200).json({ message: "Vídeo inserido com sucesso." });
     }
 }
 
